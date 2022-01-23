0a2ac6b37f51449378d019534a4d28ec660363ce
Nur <feyzanursahinli@gmail.com>
2022-01-23 06:15:50 +0100

    Java_EU8

---
+++ E8_Java/day07_Operators_UnaryShorthandRelational/Pre_Post_increment_decrement.java
@@ -0,0 +1,17 @@
+package day07_Operators_UnaryShorthandRelational;
+
+public class Pre_Post_increment_decrement {
+    public static void main(String[] args) {
+        /*
+        Pre-Increment/Decrement
+       ---------------------------
+      >> Increment/Decrement operators sre placed before the operand
+      >> Increase/decrease the value by 1 in the memory. (immediately)
+         */
+        int a=25;
+        System.out.println(++a);// 26
+        System.out.println(--a);// 26>>25
+        System.out.println("------------------------------------------");
+
+    }
+}
---
+++ E8_Java/day07_Operators_UnaryShorthandRelational/UnaryOperator.java
@@ -0,0 +1,13 @@
+package day07_Operators_UnaryShorthandRelational;
+
+public class UnaryOperator {
+    public static void main(String[] args) {
+        int num1= +25;
+        int num2= -25;
+        System.out.println(num1<0);//false
+        System.out.println(num2<0);//true
+
+
+
+    }
+}
