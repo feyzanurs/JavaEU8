e82d58f8ac0e9fa75fe0f6cd137b94268155603f
Nur <feyzanursahinli@gmail.com>
2022-02-06 13:01:22 +0100

    Java_EU8

---
+++ E8_Java/day11_SwitchStatements/SwitchStatement.java
@@ -0,0 +1,37 @@
+package day11_SwitchStatements;
+
+public class SwitchStatement {
+    public static void main(String[] args) {
+        /*
+  >> Switch-case statements used to compare the value of a variable with multiple values
+  and execute some statements based on the match.
+  >> Switch statement's expression must be evaluated to single value,
+  it cannot be long, double, float and Boolean data type.
+        *****SYNTAX*****
+        switch (Data){
+            case data1:
+                Statement;
+                break;
+            case data2:
+                Statement;
+                break;
+            default:
+               Statement;
+                break;
+
+         BREAK: used for exiting the switch, if we don't give the break statement,
+     Switch will continue to execute the next block until the break statement or } of the Switch
+         DEFAULT BLOCK: get executed if none of the blocks are matched. There can be only one default block.
+       - Switch statement cannot be used with <= >= etc.
+       or any other boolean statement, it is only for conditional statements!
+
+       - Switch statement is faster than if statement
+       - Case value must match with the case
+       - Long data type CANNOT be used with switch statement due to suffix L or l,
+     same goes for float and double
+       -BYTE, SHORT, CHAR, INT and STRING is accepted with Switch statement.
+       - Value has to be SINGLE
+         */
+
+    }
+}
