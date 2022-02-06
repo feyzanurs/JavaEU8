7031294ba0a7d5262e308cb8bb5d037e5126a6e8
Nur <feyzanursahinli@gmail.com>
2022-02-06 15:13:10 +0100

    Java_EU8

---
+++ E8_Java/day11_SwitchStatements/Calculator.java
@@ -0,0 +1,28 @@
+package day11_SwitchStatements;
+
+public class Calculator {
+    public static void main(String[] args) {
+        double n1=200.5, n2=10.5;
+        char operator='+';
+        boolean valid= operator=='+'||operator=='-'|| operator=='*'||operator=='/';
+        if (valid){
+            switch (operator){
+                case '+':
+                    System.out.println(n1+n2);
+                    break;
+                case '-':
+                    System.out.println(n1-n2);
+                    break;
+                case '*':
+                    System.out.println(n1*n2);
+                    break;
+                case '/':
+                    System.out.println(n1/n2);
+                    break;
+
+            }
+        }else{
+            System.out.println("Invalid operator");
+        }
+    }
+}
